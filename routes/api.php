5e2dae42246bd11c8738b643bc41046cccf32b05
Tobi Anifowose <tobexkee@gmail.com>
2022-05-18 19:44:08 +0100

    fix: Update the phpunit configuration

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-<?php
-
 use App\Http\Controllers\Api\LaboratoryTestController;
 use App\Http\Controllers\Api\UserLaboratoryTestController;
 use Illuminate\Support\Facades\Route;
