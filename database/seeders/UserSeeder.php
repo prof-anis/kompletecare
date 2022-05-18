4d4b9ff2d0b7a72ed4d5887e9f147e0adf87b0c0
Tobi Anifowose <tobexkee@gmail.com>
2022-05-18 19:27:39 +0100

    feat: Setup the user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /**
+         * Create a dummy user for doctor and manually setup a token this user can
+         * always use to authenticate
+         */
+        $user = User::create([
+            'name' => 'Tobi',
+            'email' => 'tobexkee@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        $user->tokens()->create([
+            'name' => "auth",
+            'token' => hash('sha256', $this->token()),
+            'abilities' => ["*"],
+        ]);
+
+        /**
+         * Create a new entry for the patient
+         */
+         User::create([
+            'name' => 'Tobi',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+
+    public function token(): string
+    {
+        return base64_encode("User Access Token");
+    }
+}
